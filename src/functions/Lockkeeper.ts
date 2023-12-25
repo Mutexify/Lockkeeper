646511d5610e8bcb61f33aa48a690c3c3fdaf5e0
kerdamon <kwalas314@gmail.com>
2023-12-25 14:37:19 +0100

    Add connection to cosmosDB

--- src/functions/Lockkeeper.ts
+++ src/functions/Lockkeeper.ts
@@ -1,19 +1,42 @@
+import { CosmosClient } from "@azure/cosmos";
 import {
   app,
   HttpRequest,
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
+import "dotenv/config";
+
+const cosmos_endpoint = process.env.COSMOS_ENDPOINT;
+const cosmos_key = process.env.COSMOS_KEY;
+if (!cosmos_endpoint || !cosmos_key) {
+  throw new Error("Cosmos DB credentials missing");
+}
+const client = new CosmosClient({ endpoint: cosmos_endpoint, key: cosmos_key });
+
+async function prepareContainer() {
+  const { database } = await client.databases.createIfNotExists({
+    id: "mutexio",
+  });
+  const { container } = await database.containers.createIfNotExists({
+    id: "slots",
+  });
+  return container;
+}
 
 export async function Lockkeeper(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
+  const container = await prepareContainer();
+  const items = await container.items.readAll().fetchAll();
 
-  const name = request.query.get("name") || (await request.text()) || "world";
-
-  return { body: `Hello, ${name}!` };
+  return {
+    body: JSON.stringify(items.resources),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 }
 
 app.http("lockkeeper", {

3bfbb6405db1cfd1765c0aaedee0746e00af78a7
kerdamon <kwalas314@gmail.com>
2024-01-28 22:55:24 +0100

    Refactor lockLogic into smaller functions

--- src/Lockkeeper/lockLogic.ts
+++ src/Lockkeeper/lockLogic.ts
@@ -1,3 +1,4 @@
+import { Container } from "@azure/cosmos";
 import {
   getItemOfId,
   patchItemOfId,
@@ -10,49 +11,63 @@ export async function maybeUpdateLock(
   slotData: SlotData
 ): Promise<LockResultData> {
   const container = await prepareCosmosContainer();
-
   const item = await getItemOfId(container, slotData.id);
 
   if (item.statusCode === 404) {
-    return {
-      result: "not_found",
-      slotData: null,
-    };
+    return notFound();
   } else if (
     item.statusCode === 200 &&
     item.resource.blocked === true &&
     slotData.blocked === true
   ) {
-    return {
-      result: "failure",
-      slotData: slotDataFromDBResponse(item.resource),
-    };
+    return failure(item.resource);
   } else if (item.statusCode === 200) {
-    const updated = await patchItemOfId(container, slotData.id, {
-      operations: [
-        {
-          op: "replace",
-          path: "/blocked",
-          value: slotData.blocked,
-        },
-      ],
-    });
+    return await patchItem(container, slotData);
+  }
+}
 
-    if (
-      updated.statusCode === 200 &&
-      updated.resource.blocked === slotData.blocked
-    ) {
-      return {
-        result: "success",
-        slotData: slotDataFromDBResponse(updated.resource),
-      };
-    }
+function notFound(): LockResultData {
+  return {
+    result: "not_found",
+    slotData: null,
+  };
+}
 
+function failure(resource: any): LockResultData {
+  return {
+    result: "failure",
+    slotData: slotDataFromDBResponse(resource),
+  };
+}
+
+async function patchItem(
+  container: Container,
+  slotData: SlotData
+): Promise<LockResultData> {
+  const updated = await patchItemOfId(container, slotData.id, {
+    operations: [
+      {
+        op: "replace",
+        path: "/blocked",
+        value: slotData.blocked,
+      },
+    ],
+  });
+
+  if (
+    updated.statusCode === 200 &&
+    updated.resource.blocked === slotData.blocked
+  ) {
     return {
-      result: "error",
-      slotData: !updated.resource
-        ? slotDataFromDBResponse(updated.resource)
-        : null,
+      result: "success",
+      slotData: slotDataFromDBResponse(updated.resource),
     };
   }
+
+  return {
+    result: "error",
+    slotData: !updated.resource
+      ? slotDataFromDBResponse(updated.resource)
+      : null,
+  };
 }
